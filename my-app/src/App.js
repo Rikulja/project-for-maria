b868f2d5dd66c3e053852a624e79c38b92a1ec35
Rikulja <platonova.tatjana92@gmail.com>
2022-12-24 21:23:31 +0200

    Add router for formpage and ampoule

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,15 +1,19 @@
-import React from "react";
 import FormPage from "./components/FormPage";
 import AmpoulePage from "./components/AmpoulePage";
 //Global Style
 import GlobalStyle from "./components/GlobalStyle";
+//Import routes
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div>
       <GlobalStyle />
 
-      <AmpoulePage />
+      <Routes>
+        <Route path="/" exact element={<FormPage />}></Route>
+        <Route path="/ampoule-vertical" exact element={<AmpoulePage />}></Route>
+      </Routes>
     </div>
   );
 }
