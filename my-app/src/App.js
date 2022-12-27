4b0f0092d37b3cc4c974370fe2ed721347b8fe4a
Rikulja <platonova.tatjana92@gmail.com>
2022-12-27 21:34:19 +0200

    change to new reactrouter api

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,22 +4,33 @@ import CountdownPage from "./components/CountdownPage";
 //Global Style
 import GlobalStyle from "./components/GlobalStyle";
 //Import routes
-import { Routes, Route } from "react-router-dom";
+import {
+  Route,
+  createBrowserRouter,
+  createRoutesFromElements,
+  RouterProvider,
+} from "react-router-dom";
+
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <>
+      <Route path="/" exact element={<FormPage />}></Route>
+      <Route path="/ampoule-vertical" exact element={<AmpoulePage />}></Route>
+      <Route
+        path="/countdown-vertical"
+        exact
+        element={<CountdownPage />}
+      ></Route>
+    </>
+  )
+);
 
 function App() {
   return (
     <div>
       <GlobalStyle />
 
-      <Routes>
-        <Route path="/" exact element={<FormPage />}></Route>
-        <Route path="/ampoule-vertical" exact element={<AmpoulePage />}></Route>
-        <Route
-          path="/countdown-vertical"
-          exact
-          element={<CountdownPage />}
-        ></Route>
-      </Routes>
+      <RouterProvider router={router} />
     </div>
   );
 }
