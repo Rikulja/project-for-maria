fcc4ca0dde76d648d02745148fefbb7098b27362
Rikulja <platonova.tatjana92@gmail.com>
2023-01-06 15:43:53 +0200

    file-saver package

--- my-app/src/components/PrintPage.js
+++ my-app/src/components/PrintPage.js
@@ -1,6 +1,35 @@
 import { Form } from "react-router-dom";
+import { saveAs, Document, Packer, Paragraph, TextRun } from "docx";
 
 const PrintPage = () => {
+  const doc = new Document({
+    sections: [
+      {
+        properties: {},
+        children: [
+          new Paragraph({
+            children: [
+              new TextRun("Hello World"),
+              new TextRun({
+                text: "Foo Bar",
+                bold: true,
+              }),
+              new TextRun({
+                text: "\tGithub is the best",
+                bold: true,
+              }),
+            ],
+          }),
+        ],
+      },
+    ],
+  });
+
+  // Used to export the file into a .docx file
+  Packer.toBlob(doc).then((blob) => {
+    // saveAs from FileSaver will download the file
+    saveAs(blob, "example.docx");
+  });
   return (
     <Form method="post">
       <div>
